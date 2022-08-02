92c7d131fa80e312f2fabffb56b96c0d8cd6595e
GuilhermeSchwengber20 <guilhermenft2021@gmail.com>
2022-08-02 17:56:45 -0300

    Terminando HTML

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,19 @@
         <input
           type="text"
           class="search-bar"
-          placeholder="Search...">
+          placeholder="Search..."
+          v-model="query">
+      </div>
+      <div class="weatherApp">
+        <div class="locationBox">
+          <div class="location"></div>
+          <div class="date"></div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp"></div>
+          <div class="weather"></div>
+        </div>
       </div>
     </main>
   </div>
@@ -15,12 +27,23 @@
 
 export default {
   name: 'App',
-  components: {
-   
-  }
+  data(){
+    return{
+      api_key: 'f6a2c317f97367bfe8fa3e5fc517e80d',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 
 </style>
